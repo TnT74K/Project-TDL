d44b77c48f17bbc3f2da71495c39417a5150e3c2
TnT74K <192174983+TnT74K@users.noreply.github.com>
2026-09-09 13:28:35 +0330

    add id to lists; implement save and load methods

--- src/definitions.py
+++ src/definitions.py
@@ -2,18 +2,19 @@ import csv
 
 
 class Task:
-    def __init__(self, name, pirority, description):  # Constructor
+    def __init__(self, name, description, pirority):  # Constructor
         self.name = name
         self.description = description
         self.pirority = pirority
 
 
 class ToDoList:
-    def __init__(self, name, tasks):
+    def __init__(self, name, tasks, id):
         self.name = name
         self.tasks = []
         self.tasks = tasks
-
+        self.id = id  # This is needed to have To-do lists with emoji \
+        # \ or special characters in their name.
 
     def add_task(self, task):
         self.tasks.append(task)
@@ -32,14 +33,20 @@ class ToDoList:
             print(f"{i}. | {task.name} | {task.pirority} | {task.description}")
         print(f" ==== ++++ ==== \n")
 
-    def save(
-        self,
-    ):
-        with open(f"../storage/{self.name}.csv", "w", newline="") as file:
+    def save_tasks(self):
+        with open(f"../storage/L-{self.id}.csv", "w", newline="") as file:
             writer = csv.writer(file)
 
-    def load(
-        self,
-    ):
-        with open(f"/storage/{self.name}.csv", "r", newline="") as file:
+            writer.writerow(["name", "description", "pirority"])
+
+        for task in self.tasks:
+            task_holder = [task.name, task.descriptions, task.pirority]
+            writer.writerow(task)
+
+    def load_tasks(self):
+        with open(f"/storage/L-{self.id}.csv", "r", newline="") as file:
             reader = csv.reader(file)
+
+            for row in reader:
+                task = Task(row[0], row[1], row[2])
+                self.tasks.append(task)
