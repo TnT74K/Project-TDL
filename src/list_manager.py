b0f2390f53a0269e524fd17640622c8ecd0fd6fb
TnT74K <192174983+TnT74K@users.noreply.github.com>
2026-09-10 16:34:37 +0330

    move list functions to separate file

---
+++ src/list_manager.py
@@ -0,0 +1,43 @@
+import csv
+from definitions import *
+
+
+def load_lists():
+    lists = []  # To save all to-do lists in a list of ToDoList objects
+
+    with open("storage/Lists.csv", "r", newline="") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:  # read all to-do lists from csv
+            tasks = []  # To save all tasks within a to-do list
+            current_list = ToDoList(row["name"], tasks, row["ID"])
+            lists.append(current_list)
+
+            with open(f"storage/L-{current_list.id}.csv") as file:
+                task_reader = csv.DictReader(file)
+
+                for row in task_reader:  # read all tasks witing a specific to-do list
+                    task = Task(row["name"], row["description"], row["pirority"])
+                    current_list.tasks.append(task)
+
+    return lists
+
+
+def save_lists(lists):
+    with open("storage/Lists.csv", "w", newline="") as lists_file:
+        list_writer = csv.writer(lists_file)
+        list_writer.writerow(["ID", "name"])  # Write headers
+
+        for current_list in lists:
+            list_writer.writerow([current_list.id, current_list.name])
+            with open(
+                f"storage/L-{current_list.id}.csv", "w", newline=""
+            ) as list_tasks_file:
+                task_writer = csv.writer(list_tasks_file)
+                task_writer.writerow(
+                    ["name", "description", "pirority"]
+                )  # Write headers
+
+                for task in current_list.tasks:
+                    task_writer.writerow([task.name, task.description, task.pirority])
+    print("Done!")
